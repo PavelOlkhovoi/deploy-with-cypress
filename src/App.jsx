4ba8e0091bc75a9186da78aed6951a8bdeaff135
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-15 19:59:14 +0200

    add background color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div className="grow w-full">
-      <div className="ml-8 text-3xl font-bold">Deploy with cypress</div>
+      <div className="ml-8 text-3xl font-bold bg-red-500">Deploy with cypress!</div>
     </div>
   );
 }
