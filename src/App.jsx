b52d5e8cd84b074fe77c8a0389b3946c88bdd506
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-15 18:04:32 +0200

    force deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div className="w-full">
-      <div className="ml-8 text-3xl font-bold text-center">Deploy with cypress</div>
+      <div className="ml-8 text-3xl font-bold">Deploy with cypress</div>
     </div>
   );
 }
