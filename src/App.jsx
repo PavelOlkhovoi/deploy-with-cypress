e6a8b23b97ed5c5aa01034bdd8af1f103f57accd
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-16 11:32:32 +0200

    change content

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ function App() {
 
   return (
 <>
-<div className="ml-8 text-3xl font-bold my-4">Deploy with cypress 1</div>
+<div className="ml-8 text-3xl font-bold my-4">Deploy with cypress 1111</div>
 <div className="ml-8 text-3xl font-bold my-4">Deploy with cypress 2</div>
 <div className="ml-8 text-3xl font-bold my-4">Deploy with cypress 3</div>
-<div className="ml-8 text-3xl font-bold my-4">Deploy with cypress 4</div>
+<div className="ml-8 text-3xl font-bold my-4">Deploy with cypress 411</div>
 </>
   );
 }
